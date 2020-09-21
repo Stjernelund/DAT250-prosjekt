47f906c050e877eada33d95903e1dab4b38f36d7
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2020-09-21 16:45:40 +0200

    Mal til rapport

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
