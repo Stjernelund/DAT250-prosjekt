594748f0b332e9c9e8ca33e4a8c6356af07a2151
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2020-10-21 13:56:08 +0200

    prøver å pulle

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from flask_wtf import Form, RecaptchaField
+from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import Required, Length, Email, EqualTo, ValidationError, Optional
 from app.models import User, Account, Log
@@ -57,6 +57,9 @@ class LoginForm(FlaskForm):
     username = StringField('Brukernavn', validators=[Required(), Length(min=5, max=20)])
     password = PasswordField('Passord', validators=[Required(), Length(min=8)])
     token = StringField('Token (2FA)', validators=[Required(), Length(6, 6)])
+    recaptcha = RecaptchaField()
+    if getattr(settings, 'DEBUG', False):
+        recaptcha.clean = lambda x: x[0]
     submit = SubmitField('Logg inn')
 
 class Editform(FlaskForm):
